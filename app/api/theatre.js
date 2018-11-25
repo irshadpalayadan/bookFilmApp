1a2b86abd2e3e9cc6d5460de50b8a6d7c90a58a5
Irshad Arippayil <iarippay@oradev.oraclecorp.com>
2018-11-25 21:46:17 +0530

    return json data

--- app/api/theatre.js
+++ app/api/theatre.js
@@ -5,7 +5,7 @@ const TheatreService = {
     
     getTheatreListByDate: function(filmName, date, location, callback) {
         // httpClient.get("api", {film:filmName, date:date, loc:location, theatreService.accountCallback(callback)});
-       TheatreTestData.getTheatreByDate(filmName, date, location);
+      return TheatreTestData.getTheatreByDate(filmName, date, location);
     }
 }
 
